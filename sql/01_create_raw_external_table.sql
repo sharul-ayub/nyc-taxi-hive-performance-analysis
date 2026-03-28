ae22a884b35ff295e48961b2ef8aec62fbf9b367
Sharul <sharulayub6@gmail.com>
2026-03-28 18:23:06 +0800

    Update 01_create_raw_external_table.sql

--- sql/01_create_raw_external_table.sql
+++ sql/01_create_raw_external_table.sql
@@ -1,1 +1,37 @@
+CREATE EXTERNAL TABLE nyctaxi_external_rawdataset (
+    VendorID INT,
+    tpep_pickup_datetime BIGINT,
+    tpep_dropoff_datetime BIGINT,
+    passenger_count BIGINT,
+    trip_distance DOUBLE,
+    RatecodeID BIGINT,
+    store_and_fwd_flag STRING,
+    PULocationID INT,
+    DOLocationID INT,
+    payment_type BIGINT,
+    fare_amount DOUBLE,
+    extra DOUBLE,
+    mta_tax DOUBLE,
+    tip_amount DOUBLE,
+    tolls_amount DOUBLE,
+    improvement_surcharge DOUBLE,
+    total_amount DOUBLE,
+    congestion_surcharge DOUBLE,
+    Airport_fee DOUBLE,
+    cbd_congestion_fee DOUBLE
+)
+STORED AS PARQUET
+LOCATION '/user/student5/project_assignment/Dataset';
 
+
+
+---
+
+### ✔ Option 3 (Best for portfolio 🔥)
+
+Make it configurable:
+
+```sql
+-- Replace with your HDFS dataset path
+-- Example: /user/your_username/project_assignment/Dataset
+LOCATION '${hdfs_dataset_path}';
